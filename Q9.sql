85f853327729bee8c26788b6884d87b97a88fe9c
ChikaraFukushima <37991247+ChikaraFukushima@users.noreply.github.com>
2018-09-21 15:44:22 +0900

    Update Q9.sql

--- Q9.sql
+++ Q9.sql
@@ -2,3 +2,12 @@ SElECT category_id
 round(category_name+item_name) as total_price
 FROM item inner join item_category
 ORDER BY total_price DESC;
+
+
+SELECT
+category_name,
+SUM(item_price) AS total_price
+FROM item_category INNER JOIN item
+on item_category.category_id = item.category_id
+GROUP BY item_category.category_id
+ORDER BY total_price DESC;
